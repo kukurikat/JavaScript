4c755ae908d278ebea67787cb59032623ee92580
kukurikat <maks2701207@gmail.com>
2024-10-18 21:30:41 +0300

    4 exrecises

--- java.js
+++ java.js
@@ -1,4 +1,19 @@
-var message = "puk";
-console.log(message);
-message = "penis";
-console.log(message);
+function calculateWordsInString(string) {
+  let words = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == " ") {
+      console;
+      words++;
+      for (let k = i; k < string.length; k++) {
+        console.log[k];
+        if (string[k + 1] != " ") {
+          i = k + 1;
+          console.log[k];
+          break;
+        }
+      }
+    }
+  }
+  console.log(words + 1);
+}
+calculateWordsInString("Some?  string, with a triple   space");
